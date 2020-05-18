63529143170db825325dd6993d2904eda7003b74
david.arteaga <david.arteaga@globant.com>
2020-05-17 21:14:16 -0300

    First commit for NAFWS

---
+++ nafws.py
@@ -0,0 +1,60 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort, make_response, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+@app.errorhandler(401)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Unauthorized'}), 401)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('index.html')
+    else:
+        return make_response(jsonify({"response":"Hello Baus!"}), 200)
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.is_json:
+        requestJson = request.get_json()
+        print("-----------------Request to login made --> ", request.get_json())
+        if requestJson['password'] == 'password' and requestJson['username'] == 'admin':
+            session['logged_in'] = True
+            return home()
+        else:
+            abort(401)
+    else:
+        abort(400) 
+
+
+def banner():
+    return """
+ .-----------------. .----------------.  .----------------.  .----------------.  .----------------. 
+| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+| | ____  _____  | || |      __      | || |  _________   | || | _____  _____ | || |    _______   | |
+| ||_   \|_   _| | || |     /  \     | || | |_   ___  |  | || ||_   _||_   _|| || |   /  ___  |  | |
+| |  |   \ | |   | || |    / /\ \    | || |   | |_  \_|  | || |  | | /\ | |  | || |  |  (__ \_|  | |
+| |  | |\ \| |   | || |   / ____ \   | || |   |  _|      | || |  | |/  \| |  | || |   '.___`-.   | |
+| | _| |_\   |_  | || | _/ /    \ \_ | || |  _| |_       | || |  |   /\   |  | || |  |`\____) |  | |
+| ||_____|\____| | || ||____|  |____|| || | |_____|      | || |  |__/  \__|  | || |  |_______.'  | |
+| |              | || |              | || |              | || |              | || |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
+
+     ----------------------------By David --> david.arteaga@globant.com------------------------
+                                                V1.0.0
+"""
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    print (banner())
+    app.run(debug=True,host='0.0.0.0', port=5000)
